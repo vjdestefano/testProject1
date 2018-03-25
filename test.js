3668069082c548d996ab771b4f40ea8dfb783b84
Vinny DeStefano <vjdestefano500@gmail.com>
2018-03-25 12:50:26 -0400

    this is working way btter

--- test.js
+++ test.js
@@ -165,7 +165,7 @@ function displayFrom3D(giff, youtube, speechText){
     var testVid = $("<iframe>").attr("src","https://www.youtube.com/embed/" + youtube);
     //testVid.attr("style","display: none;")
     testVid.attr("id", "testVid");
-    testVid.attr("width",($('#youTubeVid').innerWidth() * .9));
+    testVid.attr("width",($('#youTubeVid').innerWidth() * .95));
     testVid.attr("height",($('#youTubeVid').innerWidth() * .61))
     console.log(testVid);
     $("#youTubeVid").append(testVid);
